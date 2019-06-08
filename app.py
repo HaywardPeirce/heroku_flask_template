d7a9019c140e0b643b840cc3358cc152926381ed
Hayward <hpeirce@gmail.com>
2019-06-08 14:55:58 -0700

    HP: include template hello world

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
 from datetime import datetime
 app = Flask(__name__)
 
@@ -6,11 +7,12 @@ app = Flask(__name__)
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    # return """
+    # <h1>Hello heroku</h1>
+    # <p>It is currently {time}.</p>
+    # """.format(time=the_time)
 
-    """.format(time=the_time)
+    return render_template('index.html', the_time = the_time )
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
