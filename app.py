45ea2858507d41920b2a51eeb75ad541943ca9d1
Hayward <hpeirce@gmail.com>
2019-06-10 19:14:45 -0700

    HP: include GET and POST example

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 app = Flask(__name__)
+
+@app.route("/method", methods=['GET', 'POST'])
+def index():
+    
+    # do something if the method was GET
+    if request.method == 'GET':
+        if 'param' in request.args:
+            return 'The value of `param` was: ' + request.args['param']
+
+    # do something if the request method was POST
+    elif request.method == 'POST':
+        
+        return 'There was POST data!'
+
 
 @app.route('/')
 def homepage():
